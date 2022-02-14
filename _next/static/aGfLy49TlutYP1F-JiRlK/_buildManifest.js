4e82b6a493fce9a26d74e8e2af2610310cc66ef3
Anggun Dwiguna <gundwiguna@gmail.com>
2022-02-14 20:00:15 +0800

    next static

---
+++ _next/static/aGfLy49TlutYP1F-JiRlK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/700ba9d2aa37fed0.css","static/chunks/pages/index-46f418658db3bf3b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
